ded718bb648745f6d5716194eccc48fd456dc925
wdemichele <wdemichele@student.unimelb.edu.au>
2022-09-26 15:59:34 +1000

    added open now unit test

--- tests/unit/distance.test.js
+++ tests/unit/distance.test.js
@@ -1,4 +1,4 @@
-const dist = require('./lat-long-distance');
+const dist = require('./distance');
 
 test('calculates distance between two points', () => {
     expect(dist(53.32055555555556, 53.31861111111111, -1.7297222222222221, -1.6997222222222223)).toBe(2.004367838);
